05ffbd4ed676c4b45ed63d71fb92487a1ef5a221
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-15 13:55:09 +0300

    Add project files.

---
+++ TableCreatorExcelExporter/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Ganss.Excel;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+using TableCreatorExcelExporter.Models;
+
+namespace TableCreatorExcelExporter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new IndexViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(IndexViewModel model)
+        {
+            string[] lines = model.TextData.Split("\r\n");
+            string[] columns = lines[0].Split(";");
+
+            model.Columns.AddRange(columns);
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                List<string> oneRowData = new List<string>();
+                oneRowData.AddRange(lines[i].Split(";"));
+
+                model.Rows.Add(oneRowData);
+            }
+
+            return View(model);
+        }
+        
+        [HttpPost]
+        public IActionResult ExcelExport(string text)
+        {
+            string[] lines = text.Split("\r\n");
+            string[] columns = lines[0].Split(";");
+
+            DataTable dt = new DataTable("datatable");
+            columns.ToList().ForEach(c => dt.Columns.Add(c));
+
+            //foreach (string c in columns)
+            //{
+            //    dt.Columns.Add(c);
+            //}
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                dt.Rows.Add(lines[i].Split(";"));
+            }
+
+            ExcelMapper mapper = new ExcelMapper();
+            mapper.Save("wwwroot/data.xlsx", dt, "data");
+
+            return File("~/data.xlsx", "application/vnd.ms-excel","excel_export.xlsx");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TableCreatorExcelExporter/Models/IndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TableCreatorExcelExporter.Models
+{
+    public class IndexViewModel
+    {
+        public string TextData { get; set; }
+        public List<string> Columns { get; set; } = new List<string>();
+        public List<List<string>> Rows { get; set; } = new List<List<string>>();
+    }
+}
